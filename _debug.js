35c7cf033fad433aba78770994a9bd1367797809
avraham_shalev <avraham_shalev@ISL008704.persistent.co.in>
2017-05-03 15:41:17 +0300

    dbg

--- _debug.js
+++ _debug.js
@@ -1,2 +1,2 @@
-process.env.VCAP_SERVICES = (process.env.VCAP_SERVICES) ? process.env.VCAP_SERVICES : JSON.stringify(require("vcap.json"));
+process.env.VCAP_SERVICES = (process.env.VCAP_SERVICES) ? process.env.VCAP_SERVICES : JSON.stringify(require("./vcap.json"));
 
